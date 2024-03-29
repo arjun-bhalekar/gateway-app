949db6b368406d8eaf677ff3d1be5a57df753747
arjun-bhalekar <arjunbhalekar95@gmail.com>
2024-03-29 22:00:54 +0530

    Initial Gateway setup to route job services

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # gateway-app
 Service for Spring Cloud Gateway
+
+## Gateway Routing :
+
+spring.clout.gateway.routes[0].id=company-micro-service
+spring.clout.gateway.routes[0].uri=http://localhost:8081
+spring.clout.gateway.routes[0].predicates[0]=Path=/company/**
+
+
---
+++ src/main/java/com/sb/app/gatewayapp/GatewayAppApplication.java
@@ -0,0 +1,13 @@
+package com.sb.app.gatewayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayAppApplication.class, args);
+	}
+
+}
